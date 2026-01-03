de6837a52df70c45b1681591050c86e1b76e52ce
sagarshresti18 <sagar18reshmi@gmail.com>
2026-01-03 23:53:18 +0530

    Initial portfolio structure with sections and assets

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+function Hero (){
+    return (
+        <section>
+            <h1>Hi, I am R Sagar Shresti</h1>
+            <p>
+                Computer Science Student | AI/ML | Computer Vision | Full Stack
+            </p>
+        </section>
+    );
+}
+
+export default Hero;
